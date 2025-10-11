65bf4c3b85fafd264bd95dd8de9f160cef4f88a0
Cuong Nguyen <nguyenluan3017@gmail.com>
2025-10-11 18:17:06 +0100

    Add block variant support and enhance command-line argument parsing for matrix multiplication

--- assignment2.c
+++ assignment2.c
@@ -3,12 +3,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+#include <assert.h>
+
+#define min(a, b) ((a)>(b)?(b):(a))
 
 const char *const ARG_HELP = "--help";
 const char *const ARG_VARIANT = "--variant";
 const char *const ARG_SIZE = "--size";
+const char *const ARG_VERBOSE = "--verbose";
+const char *const ARG_BLOCK = "--block";  // Add this line
 
-const char *const VARIANT_NONBLOCK = "non-block";
+const char *const VARIANT_NAIVE = "naive";
 const char *const VARIANT_BLOCK = "block";
 const char *const VARIANT_BLAS = "blas";
 
@@ -18,6 +23,8 @@ typedef struct {
     bool flag_help;
     char flag_variant[64];
     int flag_size;
+    bool flag_verbose;
+    int flag_block;  // Add this line for block size
 } args_t;
 
 typedef struct {
@@ -27,13 +34,23 @@ typedef struct {
 
 void show_help(const char *prog_nam) {
     printf("Usage: %s [OPTIONS]\n\n", prog_nam);
+    printf("Matrix multiplication program with different implementation variants.\n\n");
     printf("Options:\n");
     printf("  --help             Show this help message and exit\n");
-    printf("  --variant VARIANT  Specify the variant to use\n");
-    printf("  --size    SIZE     An positive integer represents the size of matrix\n");
+    printf("  --variant VARIANT  Specify the multiplication variant to use\n");
+    printf("                     Available variants: naive, block, blas\n");
+    printf("  --size    SIZE     Size of the square matrices (positive integer, max %d)\n", MAX_SIZE);
+    printf("  --verbose          Enable verbose output\n");
+    printf("  --block   BLOCK    Block size for block variant (positive integer)\n");  // Add this line
+    printf("\n");
+    printf("Variants:\n");
+    printf("  naive              Standard triple-loop matrix multiplication\n");
+    printf("  block              Block-based matrix multiplication (cache-friendly)\n");
+    printf("  blas               BLAS library implementation\n");
     printf("\n");
     printf("Examples:\n");
-    printf("  %s --variant non-block --size 10\n", prog_nam);
+    printf("  %s --variant naive --size 100\n", prog_nam);
+    printf("  %s --variant block --size 512 --block 64\n", prog_nam);
     printf("  %s --help\n", prog_nam);
 }
 
@@ -41,7 +58,9 @@ args_t parse_arg(int argc, char *argv[]) {
     args_t ans = {
         false,
         {0},
-        0
+        0,
+        false,
+        0,
     };
 
     if (argc == 1) {
@@ -51,12 +70,22 @@ args_t parse_arg(int argc, char *argv[]) {
             if (strcmp(argv[i], ARG_HELP) == 0) {
                 ans.flag_help = true;   
             } else if (strcmp(argv[i], ARG_VARIANT) == 0) {
+                assert(i + 1 < argc);
                 strncpy(ans.flag_variant, argv[i + 1], sizeof ans.flag_variant);
                 i++;
             } else if (strcmp(argv[i], ARG_SIZE) == 0) {
-                char ssize[64];
+                char ssize[64] = {0};
+                assert(i + 1 < argc);
                 strncpy(ssize, argv[i + 1], sizeof ssize);
                 ans.flag_size = atoi(ssize);
+                i++;
+            } else if (strcmp(argv[i], ARG_VERBOSE) == 0) {
+                ans.flag_verbose = true;
+            } else if (strcmp(argv[i], ARG_BLOCK) == 0) {
+                char bsize[64] = {0};
+                assert(i + 1 < argc);
+                strncpy(bsize, argv[i + 1], sizeof bsize);
+                ans.flag_block = atoi(bsize);
                 i++;
             }
         }
@@ -68,11 +97,10 @@ args_t parse_arg(int argc, char *argv[]) {
 matrix_t *matrix_new(int N) {
     matrix_t *C = (matrix_t *)calloc(1, sizeof(matrix_t));
 
-    srand(time(0));
     C->size = N;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            C->mem[i][j] = rand() % 1000;
+            C->mem[i][j] = rand() % 100;
         }
     }
 
@@ -81,22 +109,31 @@ matrix_t *matrix_new(int N) {
 
 void matrix_print(matrix_t *m) {
     const int N = m->size;
+    printf("np.array([");
     for (int i = 0; i < N; i++) {
         printf("[");
         for (int j = 0; j < N; j++) {
-            printf("%7d", m->mem[i][j]);
+            printf("%d", m->mem[i][j]);
+            if (j < N - 1) {
+                printf(", ");
+            }
         }
-        printf("]\n");
+        printf("]");
+        if (i < N - 1) {
+            printf(",\n          ");
+        }
     }
+    printf("])\n");
 }
 
-matrix_t *gemm_non_block(matrix_t *A, matrix_t *B) {
+matrix_t *gemm_naive(matrix_t *A, matrix_t *B) {
     if (A->size != B->size) {
         return NULL;
     }
 
     const int N = A->size;
     matrix_t *C = (matrix_t *)calloc(1, sizeof(matrix_t));
+    C->size = N;
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -109,25 +146,57 @@ matrix_t *gemm_non_block(matrix_t *A, matrix_t *B) {
     return C;
 }
 
-void generate_matrices(int size, matrix_t **A, matrix_t **B) {
-    printf("Generating matrices of size %dx%d\n", size, size);
-    printf("Generating matrix A...");
-    *A = matrix_new(size);
-    printf("\r******** Matrix A ********\n");
-    matrix_print(*A);
+matrix_t *gemm_block(matrix_t *A, matrix_t *B, int block_size) {
+    if (A->size != B->size) {
+        return NULL;
+    }
 
-    printf("Generating matrix B...");
+    if (block_size <= 0) {
+        fprintf(stderr, "block size must be positive (receiving %d)\n", block_size);
+        exit(-1);
+    }
+
+    const int N = A->size;
+    matrix_t *C = (matrix_t *)calloc(1, sizeof(matrix_t));
+    C->size = N;
+    const int BLOCK_COUNT = N / block_size;
+
+    for (int bi = 0; bi < N; bi += block_size) {
+        for (int bj = 0; bj < N; bj += block_size) {
+            for (int i = bi; i < min(bi + block_size, N); i++) {
+                for (int j = bj; j < min(bj + block_size, N); j++) {
+                    int sum = 0;
+                    for (int k = 0; k < N; k++) {
+                        sum += A->mem[i][k] * B->mem[k][j];
+                    }
+                    C->mem[i][j] += sum;
+                }
+            }
+        }
+    }
+
+    return C;
+}
+
+void generate_matrices(bool verbose, int size, matrix_t **A, matrix_t **B) {
+    printf("Two matrices of size %dx%d\n", size, size);
+    *A = matrix_new(size);
+    if (verbose) {
+        matrix_print(*A);
+    }
     *B = matrix_new(size);
-    printf("\rMatrix B:           ");    
-    printf("\r******** Matrix B ********\n");
-    matrix_print(*B);
+    if (verbose) {
+        matrix_print(*B);
+    }
 }
 
 int main(int argc, char *argv[]) {
-    args_t args = parse_arg(argc, argv);
     matrix_t *A = NULL;
     matrix_t *B = NULL;
     matrix_t *C = NULL;
+    args_t args = parse_arg(argc, argv);
+
+    srand(time(0));
 
     if (args.flag_help) {
         show_help(argv[0]);
@@ -137,21 +206,27 @@ int main(int argc, char *argv[]) {
             return -1;
         }
 
-        generate_matrices(args.flag_size, &A, &B);
+        generate_matrices(args.flag_verbose, args.flag_size, &A, &B);
 
-        if (strcmp(args.flag_variant, VARIANT_NONBLOCK) == 0) {
-            printf("Multiplying A * B...");
-            C = gemm_non_block(A, B);
-
-            if (C != NULL) {
-                
-            }
+        if (strcmp(args.flag_variant, VARIANT_NAIVE) == 0) {
+            C = gemm_naive(A, B);
         } else if (strcmp(args.flag_variant, VARIANT_BLOCK) == 0) {
-
+            C = gemm_block(A, B, args.flag_block);
         } else if (strcmp(args.flag_variant, VARIANT_BLAS) == 0) {
-
         } else {
             printf("Unsupported variant: %s\n", args.flag_variant);
+            return -1;
+        }
+
+        if (C == NULL) {
+            return -1;
+        }
+
+        if (C == NULL) {
+            fprintf(stderr, "Can't calculate A * B");
+        } else if (args.flag_verbose) {
+            printf("The result matrix is:\n");
+            matrix_print(C);
         }
     }
 
