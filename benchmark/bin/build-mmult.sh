3a16f640f22700e907cabdd89ae59c2fd71f48da
Cuong Nguyen <nguyenluan3017@gmail.com>
2025-10-11 22:29:24 +0100

    Fix system detection in build script for macOS and Linux

--- benchmark/bin/build-mmult.sh
+++ benchmark/bin/build-mmult.sh
@@ -2,13 +2,13 @@
 
 SYS=$(uname)
 
-if [[ "$SYS" == "Darwin" ]]; then
+if [[ "$SYS" == Darwin* ]]; then
     gcc -I$(brew --prefix openblas)/include \
         -L$(brew --prefix openblas)/lib \
         -o mmult.macos \
         mmult.c  \
         -lopenblas
-elif [[ "$SYS" == "Linux" ]]; then
+elif [[ "$SYS" == Linux* ]]; then
     gcc -I/usr/local/include \
         -L/usr/local/lib \
         -o mmult.linux
