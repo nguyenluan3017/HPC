6d1c50df62dcfd3089051c2cd6aa1b0391051554
Cuong Nguyen <nguyenluan3017@gmail.com>
2025-10-24 21:47:59 +0100

    Refactor matrix norm program to improve command-line argument handling; add block size flag and enhance help output

--- matrix_norm/norm.c
+++ matrix_norm/norm.c
@@ -13,12 +13,14 @@
 
 #define DEFAULT_MATRIX_SIZE 1024
 #define DEFAULT_MIN_VALUE 1
-#define DEFAULT_MAX_VALUE 10000000
+#define DEFAULT_MAX_VALUE 1000
+#define DEFAULT_BLOCK_SIZE 512
 
 ADD_FLAG(FLAG_HELP, --help);
 ADD_FLAG(FLAG_MATRIX_SIZE, --matrix-size);
 ADD_FLAG(FLAG_MIN_VALUE, --min-value);
 ADD_FLAG(FLAG_MAX_VALUE, --max-value);
+ADD_FLAG(FLAG_BLOCK_SIZE, --block-size);
 
 typedef struct
 {
@@ -26,6 +28,7 @@ typedef struct
     size_t flag_matrix_size;
     int flag_min_value;
     int flag_max_value;
+    size_t flag_block_size;
 } args_t;
 
 typedef struct
@@ -36,8 +39,22 @@ typedef struct
 
 void show_help(const char *program_name)
 {
-    printf("%s [FLAGS]\n", program_name);
-    printf("Computing the norm of the product of NxN dense matrices on p-processor.\n");
+    printf("Usage:\n");
+    printf("  %s [FLAGS]\n\n", program_name);
+    printf("Description:\n");
+    printf("  Computes the norm of the product of NxN dense matrices on p processors.\n\n");
+
+    printf("Flags:\n");
+    printf("  %s                Show this help message.\n", FLAG_HELP);
+    printf("  %s <size>        Set matrix size (default: %d).\n", FLAG_MATRIX_SIZE, DEFAULT_MATRIX_SIZE);
+    printf("  %s <value>       Set minimum random value (default: %d).\n", FLAG_MIN_VALUE, DEFAULT_MIN_VALUE);
+    printf("  %s <value>       Set maximum random value (default: %d).\n", FLAG_MAX_VALUE, DEFAULT_MAX_VALUE);
+    printf("  %s <value>       Set block size for serial multiplication. This must divide matrix size (default: %d).\n", FLAG_BLOCK_SIZE, DEFAULT_BLOCK_SIZE);
+
+    printf("\nExamples:\n");
+    printf("  %s --matrix-size 1024 --block-size 128\n", program_name);
+    printf("  %s --matrix-size 512 --min-value 10 --max-value 1000\n", program_name);
+    printf("  %s --help\n", program_name);
 }
 
 void panic_unless(bool predicate, const char *fmt, ...)
@@ -62,6 +79,13 @@ void args_validate(args_t *args)
         args->flag_min_value, 
         args->flag_max_value
     );
+
+    panic_unless(
+        args->flag_matrix_size % args->flag_block_size == 0,
+        "Block size (%d) must divide matrix size (%d).\n",
+        args->flag_block_size,
+        args->flag_matrix_size
+    );
 }
 
 args_t args_parse(int argc, const char **argv)
@@ -71,6 +95,7 @@ args_t args_parse(int argc, const char **argv)
         .flag_matrix_size = DEFAULT_MATRIX_SIZE,
         .flag_min_value = DEFAULT_MIN_VALUE,
         .flag_max_value = DEFAULT_MAX_VALUE,
+        .flag_block_size = DEFAULT_BLOCK_SIZE,
     };
 
     if (argc == 1)
@@ -100,6 +125,11 @@ args_t args_parse(int argc, const char **argv)
             panic_unless(i + 1 < argc, "Max value must be an integer.\n");
             args.flag_max_value = atoi(argv[i + 1]);
         }
+        else if (strcmp(argv[i], FLAG_BLOCK_SIZE) == 0)
+        {
+            panic_unless(i + 1 < argc, "Block size must be an integer.\n");
+            args.flag_block_size = atoi(argv[i + 1]);
+        }
     }
 
     args_validate(&args);
@@ -115,6 +145,13 @@ matrix_t *matrix_init(size_t size)
     return mat;
 }
 
+void matrix_destroy(matrix_t **mat)
+{
+    free((*mat)->data);
+    free(*mat);
+    *mat = NULL;
+}
+
 void matrix_random(matrix_t *mat, int min_value, int max_value)
 {
     const size_t N = mat->size;
@@ -125,6 +162,33 @@ void matrix_random(matrix_t *mat, int min_value, int max_value)
             mat->data[i * N + j] = min_value + rand() % (max_value - min_value + 1);
         }
     }
+}
+
+int matrix_compare(matrix_t *lhs, matrix_t *rhs)
+{
+    const size_t N = lhs->size;
+    const double EPS = 1e-9;
+
+    panic_unless(
+        lhs->size == rhs->size, 
+        "Only compare two square matrices of the same size, not %dx%d versus %dx%d",
+        lhs->size, lhs->size,
+        rhs->size, rhs->size
+    );
+
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < N; j++)
+        {
+            const double diff = lhs->data[i * N + j] - rhs->data[i * N + j];
+            if (fabs(diff) > EPS)
+            {
+                return diff < 0 ? -1 : 1;
+            }
+        }
+    }
+
+    return 0;
 }
 
 void matrix_println(matrix_t *mat)
@@ -172,6 +236,72 @@ void matrix_println(matrix_t *mat)
     free(col_size);
 }
 
+void matrix_mult_serial(size_t block_size, matrix_t *lhs, matrix_t *rhs, matrix_t *result)
+{
+    panic_unless(
+        lhs->size == rhs->size, 
+        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d",
+        lhs->size, lhs->size,
+        rhs->size, rhs->size
+    );
+
+    const size_t N = lhs->size;
+    
+    for (size_t bi = 0; bi < N; bi += block_size)
+    {
+        for (size_t bj = 0; bj < N; bj += block_size)
+        {
+            for (size_t bk = 0; bk < N; bk += block_size)
+            {
+                int i_end = MIN(bi + block_size, N);
+                int j_end = MIN(bj + block_size, N);
+                int k_end = MIN(bk + block_size, N);
+
+                for (int k = bk; k < k_end; k++)
+                {
+                    for (int i = bi; i < i_end; i++)
+                    {
+                        const double lhs_data_ik = lhs->data[i * N + k];
+                        for (int j = bj; j < j_end; j++)
+                        {
+                            result->data[i * N + j] += lhs_data_ik * rhs->data[k * N + j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void matrix_mult_cblas(matrix_t *lhs, matrix_t *rhs, matrix_t *result)
+{
+    panic_unless(
+        lhs->size == rhs->size, 
+        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d",
+        lhs->size, lhs->size,
+        rhs->size, rhs->size
+    );
+
+    const size_t N = lhs->size;
+
+    cblas_dgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        N,
+        N,
+        N,
+        1.0,
+        lhs->data,
+        N,
+        rhs->data,
+        N,
+        0.0,
+        result->data,
+        N
+    );
+}
+
 int main(int argc, const char **argv)
 {
     args_t args = args_parse(argc, argv);
@@ -183,6 +313,42 @@ int main(int argc, const char **argv)
     }
     else
     {
+        matrix_t *A;
+        matrix_t *B;
+        matrix_t *C;
+        matrix_t *D; 
+
+        A = matrix_init(args.flag_matrix_size);
+        B = matrix_init(args.flag_matrix_size);
+        C = matrix_init(args.flag_matrix_size);
+        D = matrix_init(args.flag_matrix_size);
+
+        matrix_random(A, args.flag_min_value, args.flag_max_value);
+        matrix_random(B, args.flag_min_value, args.flag_max_value);
+
+        puts("LHS:");
+        matrix_println(A);
+        
+        puts("RHS:");
+        matrix_println(B);
+
+        puts("Serial result:");
+        matrix_mult_serial(args.flag_block_size, A, B, C);
+        matrix_println(C);
+        
+        puts("Cblas result:");
+        matrix_mult_cblas(A, B, D);
+        matrix_println(D);
+
+        if (matrix_compare(C, D) != 0)
+        {
+            printf("Discrepancy in result!");
+        }
+        
+        matrix_destroy(&A);
+        matrix_destroy(&B);
+        matrix_destroy(&C);
+        matrix_destroy(&D);
     }
     return 0;
 }
