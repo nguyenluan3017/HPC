e677ea08ca33e2c1d198159e7d76a54c92d6c461
Cuong Nguyen <nguyenluan3017@gmail.com>
2025-10-25 09:17:22 +0100

    Fix Makefile include path for OpenBLAS; update norm.c to improve flag handling and add threading support

--- matrix_norm/norm.c
+++ matrix_norm/norm.c
@@ -8,32 +8,34 @@
 #include <math.h>
 #include <time.h>
 
-#define ADD_FLAG(name, value) const char *const name = #value
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define BENCHMARK(subroutine, result) \
-    double result; \
-    { \
-        struct timespec ts_start; \
-        struct timespec ts_end; \
-        clock_gettime(CLOCK_MONOTONIC, &ts_start); \
+#define ADD_FLAG(name, value) const char *const name = value
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define BENCHMARK(subroutine, result)                                  \
+    double result;                                                     \
+    {                                                                  \
+        struct timespec ts_start;                                      \
+        struct timespec ts_end;                                        \
+        clock_gettime(CLOCK_MONOTONIC, &ts_start);                     \
         double start_time = ts_start.tv_sec + ts_start.tv_nsec * 1e-9; \
-        subroutine; \
-        clock_gettime(CLOCK_MONOTONIC, &ts_end); \
-        double end_time = ts_end.tv_sec + ts_end.tv_nsec * 1e-9; \
-        result = end_time - start_time; \
+        subroutine;                                                    \
+        clock_gettime(CLOCK_MONOTONIC, &ts_end);                       \
+        double end_time = ts_end.tv_sec + ts_end.tv_nsec * 1e-9;       \
+        result = end_time - start_time;                                \
     }
 
 #define DEFAULT_MATRIX_SIZE 1024
 #define DEFAULT_MIN_VALUE 1
 #define DEFAULT_MAX_VALUE 1000
 #define DEFAULT_BLOCK_SIZE 512
+#define DEFAULT_NUM_THREADS 4
 
-ADD_FLAG(FLAG_HELP, --help);
-ADD_FLAG(FLAG_MATRIX_SIZE, --matrix-size);
-ADD_FLAG(FLAG_MIN_VALUE, --min-value);
-ADD_FLAG(FLAG_MAX_VALUE, --max-value);
-ADD_FLAG(FLAG_BLOCK_SIZE, --block-size);
+ADD_FLAG(FLAG_HELP, "--help");
+ADD_FLAG(FLAG_MATRIX_SIZE, "--matrix-size");
+ADD_FLAG(FLAG_MIN_VALUE, "--min-value");
+ADD_FLAG(FLAG_MAX_VALUE, "--max-value");
+ADD_FLAG(FLAG_BLOCK_SIZE, "--block-size");
+ADD_FLAG(FLAG_NUMBER_OF_THREADS, "--number-of-threads");
 
 typedef struct
 {
@@ -42,6 +44,7 @@ typedef struct
     int flag_min_value;
     int flag_max_value;
     size_t flag_block_size;
+    size_t flag_number_of_threads;
 } args_t;
 
 typedef struct
@@ -50,24 +53,45 @@ typedef struct
     double *data;
 } matrix_t;
 
+typedef struct 
+{
+    matrix_t *lhs;
+    matrix_t *rhs;
+    matrix_t *result;
+    size_t block_start_index;
+    size_t block_end_index;
+    pthread_mutex_t *mutex;
+} worker_params_t; 
+
 void show_help(const char *program_name)
 {
     printf("Usage:\n");
     printf("  %s [FLAGS]\n\n", program_name);
+    
     printf("Description:\n");
-    printf("  Computes the norm of the product of NxN dense matrices on p processors.\n\n");
+    printf("  Computes the norm of the product of NxN dense matrices using various algorithms\n");
+    printf("  including naive, serial blocked, CBLAS, and parallel pthread implementations.\n\n");
 
     printf("Flags:\n");
-    printf("  %s                Show this help message.\n", FLAG_HELP);
-    printf("  %s <size>        Set matrix size (default: %d).\n", FLAG_MATRIX_SIZE, DEFAULT_MATRIX_SIZE);
-    printf("  %s <value>       Set minimum random value (default: %d).\n", FLAG_MIN_VALUE, DEFAULT_MIN_VALUE);
-    printf("  %s <value>       Set maximum random value (default: %d).\n", FLAG_MAX_VALUE, DEFAULT_MAX_VALUE);
-    printf("  %s <value>       Set block size for serial multiplication. This must divide matrix size (default: %d).\n", FLAG_BLOCK_SIZE, DEFAULT_BLOCK_SIZE);
+    printf("  %-25s Show this help message.\n", FLAG_HELP);
+    printf("  %-25s Set matrix size (default: %d).\n", FLAG_MATRIX_SIZE, DEFAULT_MATRIX_SIZE);
+    printf("  %-25s Set minimum random value (default: %d).\n", FLAG_MIN_VALUE, DEFAULT_MIN_VALUE);
+    printf("  %-25s Set maximum random value (default: %d).\n", FLAG_MAX_VALUE, DEFAULT_MAX_VALUE);
+    printf("  %-25s Set block size for serial multiplication.\n", FLAG_BLOCK_SIZE);
+    printf("  %-25s This must divide matrix size (default: %d).\n", "", DEFAULT_BLOCK_SIZE);
+    printf("  %-25s Set number of threads for parallel computation\n", FLAG_NUMBER_OF_THREADS);
+    printf("  %-25s (default: %d).\n", "", DEFAULT_NUM_THREADS);
 
     printf("\nExamples:\n");
     printf("  %s --matrix-size 1024 --block-size 128\n", program_name);
     printf("  %s --matrix-size 512 --min-value 10 --max-value 1000\n", program_name);
+    printf("  %s --number-of-threads 8 --matrix-size 2048\n", program_name);
     printf("  %s --help\n", program_name);
+    
+    printf("\nNotes:\n");
+    printf("  - Block size must evenly divide the matrix size\n");
+    printf("  - Number of threads should typically match your CPU cores\n");
+    printf("  - Larger matrices will show more significant performance differences\n");
 }
 
 void panic_unless(bool predicate, const char *fmt, ...)
@@ -80,25 +104,29 @@ void panic_unless(bool predicate, const char *fmt, ...)
     va_list args;
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
-    va_end(args);    
+    va_end(args);
     abort();
 }
 
 void args_validate(args_t *args)
 {
     panic_unless(
-        args->flag_min_value <= args->flag_max_value, 
+        args->flag_min_value <= args->flag_max_value,
         "Invalid value interval of %d .. %d\n",
-        args->flag_min_value, 
-        args->flag_max_value
-    );
+        args->flag_min_value,
+        args->flag_max_value);
 
     panic_unless(
         args->flag_matrix_size % args->flag_block_size == 0,
         "Block size (%d) must divide matrix size (%d).\n",
         args->flag_block_size,
-        args->flag_matrix_size
-    );
+        args->flag_matrix_size);
+
+    panic_unless(
+        args->flag_number_of_threads > args->flag_matrix_size,
+        "The number of threads (%d) should be no more than the matrix size (%d)\n",
+        args->flag_number_of_threads,
+        args->flag_matrix_size);        
 }
 
 args_t args_parse(int argc, const char **argv)
@@ -109,6 +137,7 @@ args_t args_parse(int argc, const char **argv)
         .flag_min_value = DEFAULT_MIN_VALUE,
         .flag_max_value = DEFAULT_MAX_VALUE,
         .flag_block_size = DEFAULT_BLOCK_SIZE,
+        .flag_number_of_threads = DEFAULT_NUM_THREADS,
     };
 
     if (argc == 1)
@@ -116,7 +145,7 @@ args_t args_parse(int argc, const char **argv)
         args.flag_help = true;
     }
 
-    for (int i = 0; i < argc; i++)
+    for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], FLAG_HELP) == 0)
         {
@@ -142,6 +171,11 @@ args_t args_parse(int argc, const char **argv)
         {
             panic_unless(i + 1 < argc, "Block size must be an integer.\n");
             args.flag_block_size = atoi(argv[i + 1]);
+        }
+        else if (strcmp(argv[i], FLAG_NUMBER_OF_THREADS) == 0)
+        {
+            panic_unless(i + 1 < argc, "The number of threads must be an unsigned integer.\n");
+            args.flag_number_of_threads = atoi(argv[i + 1]);
         }
     }
 
@@ -183,11 +217,10 @@ int matrix_compare(matrix_t *lhs, matrix_t *rhs)
     const double EPS = 1e-9;
 
     panic_unless(
-        lhs->size == rhs->size, 
-        "Only compare two square matrices of the same size, not %dx%d versus %dx%d",
+        lhs->size == rhs->size,
+        "Only compare two square matrices of the same size, not %dx%d versus %dx%d\n",
         lhs->size, lhs->size,
-        rhs->size, rhs->size
-    );
+        rhs->size, rhs->size);
 
     for (size_t i = 0; i < N; i++)
     {
@@ -218,7 +251,7 @@ void matrix_println(matrix_t *mat)
     for (size_t i = 0; i < N; i++)
     {
         for (size_t j = 0; j < N; j++)
-        {            
+        {
             sprintf(buf, "%.0f", mat->data[i * N + j]);
             buflen = strlen(buf);
             col_size[j] = MAX(col_size[j], buflen);
@@ -249,17 +282,39 @@ void matrix_println(matrix_t *mat)
     free(col_size);
 }
 
+void matrix_mult_naive(matrix_t *lhs, matrix_t *rhs, matrix_t *result)
+{
+    panic_unless(
+        lhs->size == rhs->size,
+        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d\n",
+        lhs->size, lhs->size,
+        rhs->size, rhs->size);
+
+    const size_t N = lhs->size;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            result->data[i * N + j] = 0.0;
+            for (int k = 0; k < N; k++)
+            {
+                result->data[i * N + j] += lhs->data[i * N + k] * rhs->data[k * N + j];
+            }
+        }
+    }
+}
+
 void matrix_mult_serial(size_t block_size, matrix_t *lhs, matrix_t *rhs, matrix_t *result)
 {
     panic_unless(
-        lhs->size == rhs->size, 
-        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d",
+        lhs->size == rhs->size,
+        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d\n",
         lhs->size, lhs->size,
-        rhs->size, rhs->size
-    );
+        rhs->size, rhs->size);
 
     const size_t N = lhs->size;
-    
+
     for (size_t bi = 0; bi < N; bi += block_size)
     {
         for (size_t bj = 0; bj < N; bj += block_size)
@@ -302,11 +357,10 @@ void matrix_mult_serial(size_t block_size, matrix_t *lhs, matrix_t *rhs, matrix_
 void matrix_mult_cblas(matrix_t *lhs, matrix_t *rhs, matrix_t *result)
 {
     panic_unless(
-        lhs->size == rhs->size, 
-        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d",
+        lhs->size == rhs->size,
+        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d\n",
         lhs->size, lhs->size,
-        rhs->size, rhs->size
-    );
+        rhs->size, rhs->size);
 
     const size_t N = lhs->size;
 
@@ -324,13 +378,64 @@ void matrix_mult_cblas(matrix_t *lhs, matrix_t *rhs, matrix_t *result)
         N,
         0.0,
         result->data,
-        N
-    );
+        N);
 }
 
-void matrix_mult_pthread(matrix_t *lhs, matrix_t *rhs, matrix_t *result)
+void *matrix_mult_worker(void *param)
 {
+    worker_params_t *worker_param = (worker_params_t *)param;
+
+    matrix_t *lhs = worker_param->lhs;
+    matrix_t *rhs = worker_param->rhs;
+    matrix_t *result = worker_param->result;
+
     
+}
+
+void matrix_mult_threaded(size_t num_threads, matrix_t *lhs, matrix_t *rhs, matrix_t *result)
+{
+    const size_t N = lhs->size;
+    const size_t PARTITION_SIZE = (size_t)ceil((double)N / (double)num_threads);
+    pthread_t *threads;
+    worker_params_t *worker_params;
+    pthread_mutex_t *mutex;
+
+    panic_unless(
+        lhs->size == rhs->size,
+        "I can only multiply two square matrices of the same size, not %dx%d multiply by %dx%d\n",
+        lhs->size, lhs->size,
+        rhs->size, rhs->size);
+
+    threads = (pthread_t *)calloc(num_threads, sizeof(pthread_t));
+    worker_params = (worker_params_t *)calloc(num_threads, sizeof(worker_params_t));
+    mutex = (pthread_mutex_t *)calloc(1, sizeof(pthread_mutex_t));
+
+    for (size_t i = 0; i < num_threads; i++)
+    {
+        worker_params[i].lhs = lhs;
+        worker_params[i].rhs = rhs;
+        worker_params[i].result = result;
+        worker_params[i].block_start_index = i * PARTITION_SIZE;
+        worker_params[i].block_end_index = MIN((i + 1) * PARTITION_SIZE, N);
+        worker_params[i].mutex = &mutex;
+
+        pthread_create(
+            &threads[i],
+            NULL,
+            matrix_mult_worker,
+            (void *)&worker_params[i]
+        );
+    }
+
+    for (size_t i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    free(threads);
+    free(worker_params);
+    pthread_mutex_destroy(&mutex);
+    free(mutex);
 }
 
 int main(int argc, const char **argv)
@@ -347,7 +452,7 @@ int main(int argc, const char **argv)
         matrix_t *A;
         matrix_t *B;
         matrix_t *C;
-        matrix_t *D; 
+        matrix_t *D;
 
         A = matrix_init(args.flag_matrix_size);
         B = matrix_init(args.flag_matrix_size);
@@ -357,27 +462,27 @@ int main(int argc, const char **argv)
         matrix_random(A, args.flag_min_value, args.flag_max_value);
         matrix_random(B, args.flag_min_value, args.flag_max_value);
 
-        //puts("LHS:");
-        //matrix_println(A);
-        
-        //puts("RHS:");
-        //matrix_println(B);
+        // puts("LHS:");
+        // matrix_println(A);
 
-        //puts("Serial result:");
+        // puts("RHS:");
+        // matrix_println(B);
+
+        // puts("Serial result:");
         BENCHMARK(matrix_mult_serial(args.flag_block_size, A, B, C), serial_runtime);
         printf("serial_runtime = %f\n", serial_runtime);
-        //matrix_println(C);
-        
-        //puts("Cblas result:");
-        BENCHMARK(matrix_mult_cblas(A, B, D), cblas_runtime);
-        printf("cblas_runtime = %f\n", cblas_runtime);
-        //matrix_println(D);
+        // matrix_println(C);
+
+        // puts("Cblas result:");
+        BENCHMARK(matrix_mult_naive(A, B, D), naive_runtime);
+        printf("naive_runtime = %f\n", naive_runtime);
+        // matrix_println(D);
 
         if (matrix_compare(C, D) != 0)
         {
             printf("Discrepancy in result!");
         }
-        
+
         matrix_destroy(&A);
         matrix_destroy(&B);
         matrix_destroy(&C);
