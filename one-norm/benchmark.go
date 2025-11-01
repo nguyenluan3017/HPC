a4f51f283f024481b1a9fa161a4a181a151477ce
Cuong Nguyen <nguyenluan3017@gmail.com>
2025-11-01 12:18:53 +0000

    Refactor benchmark and Makefile for improved structure and output handling

--- one-norm/benchmark.go
+++ one-norm/benchmark.go
@@ -12,18 +12,27 @@ import (
 
 const (
 	IMPL_THREADED = "threaded"
-	IMPL_SERIAL = "serial"
+	IMPL_SERIAL   = "serial"
 )
 
-type sys_info struct {
+type CPUInfo struct {
 	logicalCores uint
 	l1dCache     uint
 	l2Cache      uint
 	l3Cache      uint
 }
 
-func get_sys_info() sys_info {
-	var info sys_info
+type TestConfig struct {
+    blockSize          uint
+    numberOfThreads    *uint
+    numberOfIterations uint
+    execPath           string
+	outputFile *os.File
+	done               chan<- bool
+}
+
+func getCPUInfo() CPUInfo {
+	var info CPUInfo
 
 	switch runtime.GOOS {
 	case "darwin":
@@ -78,7 +87,7 @@ func get_sys_info() sys_info {
 	return info
 }
 
-func runNorm(matrixSize uint, blockSize uint, numberOfThreads uint, numberOfIterations uint, impl string, execPath string) {
+func runNorm(matrixSize uint, blockSize uint, numberOfThreads uint, numberOfIterations uint, impl string, execPath string, outputFile *os.File) {
 	threadedCmd := exec.Command(
 		execPath,
 		"--matrix-size", fmt.Sprint(matrixSize),
@@ -88,38 +97,80 @@ func runNorm(matrixSize uint, blockSize uint, numberOfThreads uint, numberOfIter
 		"--impl", impl,
 	)
 	threadedCmd.Stderr = os.Stderr
+	threadedCmd.Stdout = outputFile
 	fmt.Println(threadedCmd)
-	if output, err := threadedCmd.Output(); err == nil {
-		fmt.Println(string(output))
-	}
+	threadedCmd.Run()
 }
 
-func main() {
-	sysinfo := get_sys_info()
-	const blockSize = 512
-	numberOfThreads := sysinfo.logicalCores
-	const numberOfIterations = 1
-
-	execPath := flag.String("exec", "./bin/norm", "Path to exectuable to benchmark")
-	//outputDir := flag.String("output-dir", "/tmp", "Output directory for benchmark results")
-	flag.Parse()
-
+func runThreadedTest(testConfig TestConfig) {
 	for matrixSize := uint(1024); matrixSize <= 4096; matrixSize += 512 {
 		runNorm(
 			matrixSize,
-			blockSize,
-			numberOfThreads,
-			numberOfIterations,
+			testConfig.blockSize,
+			*testConfig.numberOfThreads,
+			testConfig.numberOfIterations,
 			IMPL_THREADED,
-			*execPath,
-		)
-		runNorm(
-			matrixSize,
-			blockSize,
-			numberOfThreads,
-			numberOfIterations,
-			IMPL_SERIAL,
-			*execPath,
+			testConfig.execPath,
+			testConfig.outputFile,
 		)
 	}
+	testConfig.done <- true
+}
+
+func runSerialTest(testConfig TestConfig) {
+	for matrixSize := uint(1024); matrixSize <= 4096; matrixSize += 512 {
+		runNorm(
+			matrixSize,
+			testConfig.blockSize,
+			1,
+			testConfig.numberOfIterations,
+			IMPL_SERIAL,
+			testConfig.execPath,
+			testConfig.outputFile,
+		)
+	}
+	testConfig.done <- true
+}
+
+func main() {
+	const blockSize = 512
+	const numberOfIterations = 1
+	sysinfo := getCPUInfo()
+	done := make(chan bool)	
+	execPath := flag.String("exec", "./bin/norm", "Path to exectuable to benchmark")
+	outputDir := flag.String("output-dir", "/tmp", "Output directory for benchmark results")
+
+	flag.Parse()
+
+	threadedOutputFile, err := os.OpenFile(*outputDir + "/threaded_results.yaml", os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer threadedOutputFile.Close()
+
+	serialOutputFile, err := os.OpenFile(*outputDir + "/serial_results.yaml", os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer serialOutputFile.Close()
+
+	go runThreadedTest(TestConfig{
+		blockSize: blockSize,
+		numberOfThreads: &sysinfo.logicalCores,
+		numberOfIterations: numberOfIterations,
+		execPath: *execPath,
+		done: done,
+		outputFile: threadedOutputFile,
+	})
+
+	go runSerialTest(TestConfig{
+		blockSize: blockSize,
+		numberOfIterations: numberOfIterations,
+		execPath: *execPath,
+		done: done,
+		outputFile: serialOutputFile,
+	})
+
+	<- done
+	<- done
 }
